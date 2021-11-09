0b0b8cc7399c81ab3c404f39746738f5e48527aa
Jean-Pierre Van Loggerenberg <jploggies@gmail.com>
2021-11-09 21:29:19 +0100

    Implementation of Steering.
    Implementation of Steer() in Car.cs
    Added a canvas with two images that take in events to handle left and right steering
    Added Serialized turnSpeed method

--- Mobile3DCarGame/Assets/Scripts/Car.cs
+++ Mobile3DCarGame/Assets/Scripts/Car.cs
@@ -8,10 +8,21 @@ public class Car : MonoBehaviour
     [SerializeField] private float speed = 10f;
     [SerializeField] private float speedGainPerSecond = 0.2f;
   
+    [SerializeField] private float turnSpeed = 200f;
+    private int steerValue;
+
     // Update is called once per frame
     void Update()
     {
         speed += speedGainPerSecond * Time.deltaTime;
+
         transform.Translate(Vector3.forward * speed * Time.deltaTime);
+
+        transform.Rotate(0f, steerValue * turnSpeed * Time.deltaTime, 0f);
+    }
+
+    public void Steer(int value)
+    {
+        steerValue = value;
     }
 }
