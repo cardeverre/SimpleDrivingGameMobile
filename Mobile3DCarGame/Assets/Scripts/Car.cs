36402f030e23091bf26a00f3a975c097978b7df3
Jean-Pierre Van Loggerenberg <jploggies@gmail.com>
2021-11-09 20:52:02 +0100

    Added Car Object and Simple Script to Increase speed

---
+++ Mobile3DCarGame/Assets/Scripts/Car.cs
@@ -0,0 +1,17 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Car : MonoBehaviour
+{
+
+    [SerializeField] private float speed = 10f;
+    [SerializeField] private float speedGainPerSecond = 0.2f;
+  
+    // Update is called once per frame
+    void Update()
+    {
+        speed += speedGainPerSecond * Time.deltaTime;
+        transform.Translate(Vector3.forward * speed * Time.deltaTime);
+    }
+}
