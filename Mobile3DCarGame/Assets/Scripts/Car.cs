9ae7d0143e2b14e0c3669f51fc2120c5894489f8
Jean-Pierre Van Loggerenberg <jploggies@gmail.com>
2021-11-09 21:55:53 +0100

    Multiple Additions
    Added new Scene for Main Menu, renamed the default scene to game_scene
    Added box collider to the car
    Added tags tot he obstacles
    Added method to update trigger when there is car vs obstacle collider

--- Mobile3DCarGame/Assets/Scripts/Car.cs
+++ Mobile3DCarGame/Assets/Scripts/Car.cs
@@ -1,13 +1,14 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class Car : MonoBehaviour
 {
 
     [SerializeField] private float speed = 10f;
     [SerializeField] private float speedGainPerSecond = 0.2f;
-  
+
     [SerializeField] private float turnSpeed = 200f;
     private int steerValue;
 
@@ -19,6 +20,15 @@ public class Car : MonoBehaviour
         transform.Translate(Vector3.forward * speed * Time.deltaTime);
 
         transform.Rotate(0f, steerValue * turnSpeed * Time.deltaTime, 0f);
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.CompareTag("Obstacle"))
+        {
+            SceneManager.LoadScene(0);
+        }
+
     }
 
     public void Steer(int value)
